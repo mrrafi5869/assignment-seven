c021abbde8cb447c143657df8d3d938443b22a19
Mohammad Rafe <mrrafi5869@gmail.com>
2022-09-15 12:45:08 +0600

    error count

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ const typeController = (e) => {
 
   // if it is not a valid character like Control/Alt then skip displaying anything
   if (!validLetters.includes(newLetter)) {
+    errorCount++
     return;
   }
 
@@ -81,7 +82,7 @@ const gameOver = () => {
   resultModal.innerHTML += `
     <h1>Finished!</h1>
     <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+    <p>You made <span class="bold red">${errorCount ? errorCount++ : errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
 
